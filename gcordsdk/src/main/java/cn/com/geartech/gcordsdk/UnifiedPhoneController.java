a935aec91c0ced9348f2c93b03b20e44aa509863
kya <zhenqiang.gao@geartech.com>
2021-12-22 10:21:58 +0800

    fix areacode for 安徽巢湖

--- gcordsdk/src/main/java/cn/com/geartech/gcordsdk/UnifiedPhoneController.java
+++ gcordsdk/src/main/java/cn/com/geartech/gcordsdk/UnifiedPhoneController.java
@@ -1461,13 +1461,21 @@ public class UnifiedPhoneController {
                         public void onGetAreaCode(AreaCodeItem areaCodeItem) {
                             String result = number;
 
-                            if (areaCodeItem != null
-                                    && !currentAreaCode.equals(areaCodeItem.getAreaCode())) {
-                                if (number.length() == 11
+                            if (areaCodeItem != null){
+
+                                boolean shouldAdd = false;
+                                if(!TextUtils.isEmpty(areaCodeItem.getAreaCode()) && !currentAreaCode.equals(areaCodeItem.getAreaCode())) {
+                                    shouldAdd = true;
+                                }else if(!TextUtils.isEmpty(areaCodeItem.getCity()) && ! AreaCodeIndex.getCity(currentAreaCode).contains(areaCodeItem.getCity())){
+                                    shouldAdd = true;
+                                }
+
+                                if (shouldAdd && number.length() == 11
                                         && number.startsWith("1")) {
                                     //异地手机号加0
                                     result = "0".concat(number);
                                 }
+
                             }
                             if (listener != null) {
                                 String dialNumber = checkDialPrefix(result);
